f1e6332d2fd2dc8c8d602948e1964e3da9163b26
lkw5566 <lkw5566@gmail.com>
2023-12-20 17:30:40 +0900

    commit!!

---
+++ study/vue-todo/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data() {
+    return {
+      todoItems : []
+    }
+  },
+  methods : {
+    addTodo(todoItem) {
+      // 브라우저 로컬 스토리지에 저장
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  components : {
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
+  }
+}
+</script>
+
+<style>
+  body {
+    text-align : center;
+    background-color : #F6F6F8;
+  }
+  input {
+    border-style : groove;
+    width : 200px;
+  }
+  button {
+    border-style : groove;
+  }
+  .shados {
+    box-shadow : 5px 10px 10px rgba(0, 0, 0, 0.03                                                                                                                                                                                                                                                                                                   d)
+  }
+</style>
